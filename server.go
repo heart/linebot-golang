f946aacb4d57c3e6d083fda42f108c2aced2dc29
narongrit kanhanoi <narongrit@3dsinteractive.com>
2018-07-10 12:51:33 +0700

    create webhook url

--- server.go
+++ server.go
@@ -3,15 +3,35 @@ import (
   "net/http"
   "strings"
 )
+
 func sayHello(w http.ResponseWriter, r *http.Request) {
   message := r.URL.Path
   message = strings.TrimPrefix(message, "/")
   message = "Hello " + message
   w.Write([]byte(message))
 }
+
+func webhook(w http.ResponseWriter, r *http.Request) {
+	defer req.Body.Close()
+	body, err := ioutil.ReadAll(req.Body)
+	if err != nil {
+	    // ...
+	}
+	decoded, err := base64.StdEncoding.DecodeString(req.Header.Get("X-Line-Signature"))
+	if err != nil {
+	    // ...
+	}
+	hash := hmac.New(sha256.New, []byte("<channel secret>"))
+	hash.Write(body)
+	// Compare decoded signature and `hash.Sum(nil)` by using `hmac.Equal`
+}
+
 func main() {
   http.HandleFunc("/", sayHello)
+  http.HandleFunc("/webhook", webhook)
+
   if err := http.ListenAndServe(":8080", nil); err != nil {
     panic(err)
   }
 }
+
